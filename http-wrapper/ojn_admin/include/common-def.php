982992c75dfc6fc7596ed629cb2ebfc0f6afed99
Matthieu Gonnet <matthieu.gonnet@gmail.com>
2019-09-27 14:32:39 +0200

    [Fix] Cannot login with Php7.
          - PHP7 Warning:  session_start() expects parameter 1 to be array

--- http-wrapper/ojn_admin/include/common-def.php
+++ http-wrapper/ojn_admin/include/common-def.php
@@ -4,7 +4,7 @@ define('ROOT_WWW_ADMIN', 'http://<HOSTNAME>/ojn_admin/');
 define('ROOT_WWW_API', 'http://<HOSTNAME>/ojn_api/');
 define('ADMIN_EMAIL', '<EMAIL>');
 
-session_start('openJabNab');
+session_start(['openJabNab']);
 require_once(ROOT_SITE.'class/api.class.php');
 require_once(ROOT_SITE.'class/template.class.php');
 $ojnAPI = new ojnApi();
